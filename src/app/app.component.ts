0e73ec893673077b4adaa8d2b8bbafbe83075ff5
jirayu <jirayu.photisan@gmail.com>
2018-01-27 15:07:23 +0700

    init for lap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  public pattern: string = "";
+  public leftOperand: number;
+  public operand: number;
+  public rightOperand: number;
+  public result: string;
+
+  ngOnInit() {
+  }
+
+  captcha = () => {
+    this.result = "?";
+  }
+
 }
