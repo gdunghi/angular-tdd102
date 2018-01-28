6ec0459bb80e1a4024cc7f072ba53daf0b856e0b
jirayu <jirayu.photisan@gmail.com>
2018-01-28 11:05:55 +0700

    refactor nunberToString

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,28 +23,7 @@ export class AppComponent implements OnInit {
   }
 
   numberToString = (num: Number): string => {
-    if (num === 1) {
-      return "1";
-    } else if (num === 9) {
-      return "9";
-    } else if (num === 0) {
-      return "0";
-    } else if (num === 7) {
-      return "7";
-    } else if (num === 8) {
-      return "8";
-    } else if (num === 2) {
-      return "2";
-    } else if (num === 3) {
-      return "3";
-    } else if (num === 4) {
-      return "4";
-    } else if (num === 5) {
-      return "5";
-    } else if (num === 6) {
-      return "6";
-    }
-    return "1";
+    return num + "";
   }
 
 }
